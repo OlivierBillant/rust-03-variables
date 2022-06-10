cf376859b977c9386274f2506a4e04fb9b92c995
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-10 14:04:54 +0200

    ADD : setting variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let mut x: i32 = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const USERS_CONNECTED: u32 = 10_000;
+    println!("La constante vaut: {}", USERS_CONNECTED);
+    let y: i32 = 50;
+    println!("The value of x is: {}", y);
+    let y: i32 = 60;
+    println!("The value of x is: {}", y);
+    let y: &str = "sixty";
+    println!("The value of x is: {}", y);
+
+// Datatypes
+//     Integers
+//     Floating-point numbers
+//     Booleans
+//     Character
+
 }
