5a3be9389facac4e7ea6b52caa716e09ad7f491b
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-08-05 11:42:41 +0200

    ADD : functions

--- rust-fundamentals.md
+++ rust-fundamentals.md
@@ -1,2 +1,15 @@
 # Rust notions
 Variables are immutable by default.
+
+- Datatypes
+- Integers
+- Floating-point numbers
+- Booleans
+- Character
+
+Constants are always immutable and must be typed.
+
+Shadowing allow you to create a new variable using the same name of a previous one.  
+
+<br>
+Functions must be all lowercase.
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ fn main() {
     x = 6;
     println!("The value of x is: {}", x);
 
-    const USERS_CONNECTED: u32 = 10_000;
-    println!("La constante vaut: {}", USERS_CONNECTED);
+    const TEST_CONSTANT: u32 = 10_000;
+    println!("The const value is: {}", TEST_CONSTANT);
     let y: i32 = 50;
     println!("The value of x is: {}", y);
     let y: i32 = 60;
     println!("The value of x is: {}", y);
     let y: &str = "sixty";
     println!("The value of x is: {}", y);
+    let y:i32 = 60;
+    println!("The sum of x and y is {}", my_function(x, y));
+}
 
-// Datatypes
-//     Integers
-//     Floating-point numbers
-//     Booleans
-//     Character
-
+fn my_function(x: i32, y:i32)-> i32{
+    println!("A new function called up in main !");
+    return x+y;
 }
